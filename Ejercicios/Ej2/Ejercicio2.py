0056bb0e41006e10e900b27b89c7f7d2e1a4bc08
Agirre8 <inagi30@gmail.com>
2022-10-28 16:57:51 +0200

    ejercicio5

--- Ejercicios/Ej2/Ejercicio2.py
+++ Ejercicios/Ej2/Ejercicio2.py
@@ -1,23 +1,21 @@
 
-
-#Funcion que hace los calculos de la regla se Sarrus
-lista = []
-def determinanteSarrus(self):
-    for l in range(1, 4):
-        for c in range(1, 4):
-            matriz = int(input(f"[Inserte un valor [{l}, {c}] "))
-            lista.append(matriz)
-    primero = (lista[0] * lista[4] * lista[8])
-    segundo = (lista[1] * lista[5] * lista[6])
-    tercero = (lista[2] * lista[3] * lista[7])
-    primero2 = (lista[2] * lista[4] * lista[6])
-    segundo2 = (lista[0] * lista[5] * lista[7])
-    tercero2 = (lista[1] * lista[3] * lista[8])
-
-    sum = ((primero + segundo + tercero) - (primero2 + segundo2 + tercero2))
+def getcofactor(m, i, j):
+    return [row[: j] + row[j+1:] for row in (m[: i] + m[i+1:])]
 
 
-print(f"[El resultado es =[m")
-for c in range(len(lista)):
-    print(f"[{lista[c] }]",end='\n' if c == 2 or c == 5 else "")
-print(f'\n[1;34mdet(a) = [1;35m{determinanteSarrus.sum}',end='')
+def determinantOfMatrix(mat):
+ 
+    if(len(mat) == 2):
+        value = mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
+        return value
+
+    Sum = 0
+
+    for current_column in range(len(mat)):
+
+        sign = (-1) ** (current_column)
+        sub_det = determinantOfMatrix(getcofactor(mat, 0, current_column))
+        Sum += (sign * mat[0][current_column] * sub_det)
+
+    return Sum
+ 
--- Ejercicios/lanzador.py
+++ Ejercicios/lanzador.py
@@ -1,5 +1,6 @@
 from turtle import end_fill
 from Ejercicios.Ej1 import Ejercicio1 as ej1
+from Ejercicios.Ej2 import Ejercicio2 as ej2
 
 def ejecutar():
     n = input("Inserte el numero de el ejercicio que quieras ejecutar:")
@@ -9,7 +10,14 @@ def ejecutar():
         ej1.FuncionOrdenar(n,'A','B','C')
 
     elif n==2:
-        return
+            
+        mat = [[1, 0, 2, -1],
+            [3, 0, 0, 5],
+            [2, 1, 4, -3],
+            [1, 0, 5, 0]]
+
+        print('El determinante de la matriz es:', ej2.determinantOfMatrix(mat))
+
 
     elif n==3:
         return
