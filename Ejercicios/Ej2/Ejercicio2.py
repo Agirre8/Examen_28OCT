724acf9ef6e0af806d0ad2713a0d858850037bfe
Agirre8 <inagi30@gmail.com>
2022-10-28 16:49:38 +0200

    ejercicio2

--- Ejercicios/Ej2/Ejercicio2.py
+++ Ejercicios/Ej2/Ejercicio2.py
@@ -1,30 +1,23 @@
-import numpy as np
-def crear_matriz(self):
-    
-    self.matriz = np.array([[55, 25, 15], 
-                    [30, 44, 2], 
-                    [11, 45, 77]])
 
-def determinant_fast(A):
-    # Section 1: Establish n parameter and copy A
-    n = len(A)
-    AM = crear_matriz(A)
- 
-    # Section 2: Row ops on A to get in upper triangle form
-    for fd in range(n): # A) fd stands for focus diagonal
-        for i in range(fd+1,n): # B) only use rows below fd row
-            if AM[fd][fd] == 0: # C) if diagonal is zero ...
-                AM[fd][fd] == 1.0e-18 # change to ~zero
-            # D) cr stands for "current row"
-            crScaler = AM[i][fd] / AM[fd][fd] 
-            # E) cr - crScaler * fdRow, one element at a time
-            for j in range(n): 
-                AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
-     
-    # Section 3: Once AM is in upper triangle form ...
-    product = 1.0
-    for i in range(n):
-        # ... product of diagonals is determinant
-        product *= AM[i][i] 
- 
-    return product
+
+#Funcion que hace los calculos de la regla se Sarrus
+lista = []
+def determinanteSarrus(self):
+    for l in range(1, 4):
+        for c in range(1, 4):
+            matriz = int(input(f"[Inserte un valor [{l}, {c}] "))
+            lista.append(matriz)
+    primero = (lista[0] * lista[4] * lista[8])
+    segundo = (lista[1] * lista[5] * lista[6])
+    tercero = (lista[2] * lista[3] * lista[7])
+    primero2 = (lista[2] * lista[4] * lista[6])
+    segundo2 = (lista[0] * lista[5] * lista[7])
+    tercero2 = (lista[1] * lista[3] * lista[8])
+
+    sum = ((primero + segundo + tercero) - (primero2 + segundo2 + tercero2))
+
+
+print(f"[El resultado es =[m")
+for c in range(len(lista)):
+    print(f"[{lista[c] }]",end='\n' if c == 2 or c == 5 else "")
+print(f'\n[1;34mdet(a) = [1;35m{determinanteSarrus.sum}',end='')
