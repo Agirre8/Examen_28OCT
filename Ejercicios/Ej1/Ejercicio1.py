76f090a4139279885de916b909ba2582002e9f1b
Agirre8 <inagi30@gmail.com>
2022-10-28 16:34:18 +0200

    ejercicio1 y lanzador1

---
+++ Ejercicios/Ej1/Ejercicio1.py
@@ -0,0 +1,23 @@
+def FuncionOrdenar(n , inicio, destino, auxiliar):
+    if n==1:
+        print (f"Mover disco desde inicio {inicio} a su destino{destino}")
+        return
+    FuncionOrdenar(n-1, inicio, auxiliar, destino)
+    print (f"Mover disco {n} desde inicio {inicio} al destino {destino}")
+    FuncionOrdenar(n-1, auxiliar, destino, inicio)
+
+n=5
+FuncionOrdenar(n,'A','B','C')
+
+   
+
+
+
+
+
+    
+
+
+
+
+  
--- Ejercicios/Ej2/Ejercicio2.py
+++ Ejercicios/Ej2/Ejercicio2.py
@@ -5,3 +5,26 @@ def crear_matriz(self):
                     [30, 44, 2], 
                     [11, 45, 77]])
 
+def determinant_fast(A):
+    # Section 1: Establish n parameter and copy A
+    n = len(A)
+    AM = copy_matrix(A)
+ 
+    # Section 2: Row ops on A to get in upper triangle form
+    for fd in range(n): # A) fd stands for focus diagonal
+        for i in range(fd+1,n): # B) only use rows below fd row
+            if AM[fd][fd] == 0: # C) if diagonal is zero ...
+                AM[fd][fd] == 1.0e-18 # change to ~zero
+            # D) cr stands for "current row"
+            crScaler = AM[i][fd] / AM[fd][fd] 
+            # E) cr - crScaler * fdRow, one element at a time
+            for j in range(n): 
+                AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
+     
+    # Section 3: Once AM is in upper triangle form ...
+    product = 1.0
+    for i in range(n):
+        # ... product of diagonals is determinant
+        product *= AM[i][i] 
+ 
+    return product
