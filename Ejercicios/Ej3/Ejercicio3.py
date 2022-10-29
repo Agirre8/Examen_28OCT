2584644113d7ea01814b2dee24f39b5c1fcd65e7
Agirre8 <inagi30@gmail.com>
2022-10-29 06:45:44 +0200

    + cambios

--- Ejercicios/Ej3/Ejercicio3.py
+++ Ejercicios/Ej3/Ejercicio3.py
@@ -1,5 +1,18 @@
 from nave import *
-        
+import pathlib
+import csv
+
+actual_path = pathlib.Path(__file__).parent.absolute()
+
+
+with open('naves.csv', newline='') as f:
+    reader = csv.reader(f)
+    for naves in reader:
+        print(naves)
+
+def imprimirNaves():
+    return print(naves)
+
 def ordenar_nombres():
     nombres = [a.nombre, b.nombre, c.nombre, d.nombre, e.nombre]
     nombres.sort()
@@ -7,12 +20,21 @@ def ordenar_nombres():
     nombres.reverse()
     print(nombres)
 
+
 a = Nave("Estrella de la muerte", 500, 8998, 69420)
 b = Nave("Halcon milenario", 1234, 1059, 1919191)
 c = Nave("Lanzador imperial", 9999, 222, 37201)
 d = Nave("Destructor estelar", 3721, 3939, 77777)
 e = Nave("Supremacy", 56565, 12198, 66666)
-ordenar_nombres()
+
+
+if __name__ == "__main__":
+        
+    """"
+    ordenar_nombres()
+    """
+
+    imprimirNaves()
 
 
 
