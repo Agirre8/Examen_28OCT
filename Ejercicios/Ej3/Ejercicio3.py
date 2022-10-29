9b3140ee24a24916e452ebe1373ad21a8ba256e5
Agirre8 <inagi30@gmail.com>
2022-10-29 07:06:11 +0200

    cambios leercsv

--- Ejercicios/Ej3/Ejercicio3.py
+++ Ejercicios/Ej3/Ejercicio3.py
@@ -1,24 +1,45 @@
 from nave import *
 import pathlib
 import csv
+from collections import defaultdict
 
 actual_path = pathlib.Path(__file__).parent.absolute()
-
-
 with open('naves.csv', newline='') as f:
     reader = csv.reader(f)
-    for naves in reader:
-        print(naves)
+    naves = list(reader)
+
+def leerCSV_1():
+    columns = defaultdict(list) # each value in each column is appended to a list
+
+    with open('naves.csv') as f:
+        reader = csv.DictReader(f) # read rows into a dictionary format
+        for row in reader: # read a row as {column1: value1, column2: value2,...}
+            for (k,v) in row.items(): # go over each column name and value 
+                columns[k].append(v)
 
 def imprimirNaves():
     return print(naves)
 
 def ordenar_nombres():
+    
     nombres = [a.nombre, b.nombre, c.nombre, d.nombre, e.nombre]
     nombres.sort()
     print(nombres)
     nombres.reverse()
     print(nombres)
+
+def ordenar_nombres_1():
+    nombres = [df.iloc[:, 0]]
+    for i in naves:
+        nombres.append(i["Nombre"])
+    nombres.sort()
+    print(nombres)
+    
+def ordenar_nombres_2():
+    nombres = []
+    for i in naves:
+        nombres.reverse(i.nombre)
+        print(nombres)
 
 
 a = Nave("Estrella de la muerte", 500, 8998, 69420)
@@ -35,6 +56,7 @@ if __name__ == "__main__":
     """
 
     imprimirNaves()
+    ordenar_nombres_1()
 
 
 
